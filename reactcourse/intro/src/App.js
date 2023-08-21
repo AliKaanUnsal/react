f427bfebff780644f3d2519bdd28b9bf31542453
AliKaan_Ünsal <106932921+alikaan321@users.noreply.github.com>
2023-08-21 18:59:45 +0300

    react

--- reactcourse/intro/src/App.js
+++ reactcourse/intro/src/App.js
@@ -1,37 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import { Col, Container, Row } from "reactstrap";
 
-
 export default class App extends Component {
-  state={currentCategory:""}
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
 
   changeCategory = (category) => {
-    this.setState({ currentCategory: category.categoryName })
-};
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
   render() {
-   let productInfo = { title: "Productlist" }
-   let categoryInfo = { title: "CategoryList" }
+    let productInfo = { title: "Productlist" };
+    let categoryInfo = { title: "CategoryList" };
     return (
       <div>
         <Container>
-          <Row>
-            <Navi />
-          </Row>
+          <Navi />
           <Row>
             <Col xs="3">
-              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
-              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
             </Col>
           </Row>
         </Container>
-
       </div>
     );
   }
 }
-
